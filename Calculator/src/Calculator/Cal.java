200bf917c680513739b80c7d00208bf73aeba318
sheikhsarafathossain <sheikhsarafathossain@gmail.com>
2024-08-12 18:28:27 +0600

    Dark Theme Calculator Update With Jar File

--- Calculator/src/Calculator/Cal.java
+++ Calculator/src/Calculator/Cal.java
@@ -11,6 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.DropMode;
 import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.UIManager;
 
 public class Cal {
 
@@ -51,17 +53,25 @@ public class Cal {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(0, 0, 0));
+		frame.setBackground(new Color(0, 0, 0));
+		frame.getContentPane().setForeground(new Color(128, 128, 128));
 		frame.setBounds(100, 100, 473, 570);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
+		textField.setForeground(new Color(255, 255, 255));
+		textField.setBackground(new Color(0, 0, 0));
 		textField.setFont(new Font("Tahoma", Font.BOLD, 42));
 		textField.setBounds(10, 11, 438, 141);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("AC");
+		btnNewButton.setBackground(Color.GRAY);
+		btnNewButton.setForeground(new Color(0, 0, 0));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(null);
@@ -72,6 +82,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("\uF0E7");
+		btnNewButton_1.setBackground(Color.GRAY);
+		btnNewButton_1.setForeground(new Color(0, 0, 0));
 		btnNewButton_1.setFont(new Font("Wingdings", Font.BOLD, 18));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -89,6 +101,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("%");
+		btnNewButton_2.setBackground(Color.GRAY);
+		btnNewButton_2.setForeground(new Color(0, 0, 0));
 		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 18));
 		btnNewButton_2.setBounds(234, 177, 102, 60);
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -101,6 +115,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JButton btnNewButton_1_1 = new JButton("÷");
+		btnNewButton_1_1.setBackground(Color.ORANGE);
+		btnNewButton_1_1.setForeground(Color.WHITE);
 		btnNewButton_1_1.setFont(new Font("Tahoma", Font.BOLD, 18));
 		btnNewButton_1_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -113,6 +129,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_1);
 		
 		JButton btnNewButton_3 = new JButton("7");
+		btnNewButton_3.setBackground(Color.DARK_GRAY);
+		btnNewButton_3.setForeground(Color.WHITE);
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_3.getText();
@@ -124,6 +142,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_3);
 		
 		JButton btnNewButton_1_2 = new JButton("8");
+		btnNewButton_1_2.setBackground(Color.DARK_GRAY);
+		btnNewButton_1_2.setForeground(Color.WHITE);
 		btnNewButton_1_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_1_2.getText();
@@ -135,6 +155,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_2);
 		
 		JButton btnNewButton_2_1 = new JButton("9");
+		btnNewButton_2_1.setBackground(Color.DARK_GRAY);
+		btnNewButton_2_1.setForeground(Color.WHITE);
 		btnNewButton_2_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_2_1.getText();
@@ -146,6 +168,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_2_1);
 		
 		JButton btnNewButton_1_1_1 = new JButton("×");
+		btnNewButton_1_1_1.setBackground(Color.ORANGE);
+		btnNewButton_1_1_1.setForeground(Color.WHITE);
 		btnNewButton_1_1_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				number1 = Double.parseDouble(textField.getText());
@@ -158,6 +182,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_1_1);
 		
 		JButton btnNewButton_4 = new JButton("4");
+		btnNewButton_4.setBackground(Color.DARK_GRAY);
+		btnNewButton_4.setForeground(Color.WHITE);
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_4.getText();
@@ -169,6 +195,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_4);
 		
 		JButton btnNewButton_1_3 = new JButton("5");
+		btnNewButton_1_3.setForeground(Color.WHITE);
+		btnNewButton_1_3.setBackground(Color.DARK_GRAY);
 		btnNewButton_1_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_1_3.getText();
@@ -180,6 +208,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_3);
 		
 		JButton btnNewButton_2_2 = new JButton("6");
+		btnNewButton_2_2.setBackground(Color.DARK_GRAY);
+		btnNewButton_2_2.setForeground(Color.WHITE);
 		btnNewButton_2_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_2_2.getText();
@@ -191,6 +221,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_2_2);
 		
 		JButton btnNewButton_1_1_2 = new JButton("-");
+		btnNewButton_1_1_2.setBackground(Color.ORANGE);
+		btnNewButton_1_1_2.setForeground(Color.WHITE);
 		btnNewButton_1_1_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				number1 = Double.parseDouble(textField.getText());
@@ -203,6 +235,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_1_2);
 		
 		JButton btnNewButton_2_2_1 = new JButton("3");
+		btnNewButton_2_2_1.setBackground(Color.DARK_GRAY);
+		btnNewButton_2_2_1.setForeground(Color.WHITE);
 		btnNewButton_2_2_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_2_2_1.getText();
@@ -214,6 +248,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_2_2_1);
 		
 		JButton btnNewButton_1_3_1 = new JButton("2");
+		btnNewButton_1_3_1.setBackground(Color.DARK_GRAY);
+		btnNewButton_1_3_1.setForeground(Color.WHITE);
 		btnNewButton_1_3_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_1_3_1.getText();
@@ -225,6 +261,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_3_1);
 		
 		JButton btnNewButton_1_1_2_1 = new JButton("+");
+		btnNewButton_1_1_2_1.setBackground(Color.ORANGE);
+		btnNewButton_1_1_2_1.setForeground(Color.WHITE);
 		btnNewButton_1_1_2_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				number1 = Double.parseDouble(textField.getText());
@@ -237,6 +275,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_1_2_1);
 		
 		JButton btnNewButton_4_1 = new JButton("1");
+		btnNewButton_4_1.setBackground(Color.DARK_GRAY);
+		btnNewButton_4_1.setForeground(Color.WHITE);
 		btnNewButton_4_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_4_1.getText();
@@ -248,6 +288,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_4_1);
 		
 		JButton btnNewButton_2_2_2 = new JButton(".");
+		btnNewButton_2_2_2.setBackground(Color.DARK_GRAY);
+		btnNewButton_2_2_2.setForeground(Color.WHITE);
 		btnNewButton_2_2_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_2_2_2.getText();
@@ -259,6 +301,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_2_2_2);
 		
 		JButton btnNewButton_1_1_2_2 = new JButton("=");
+		btnNewButton_1_1_2_2.setBackground(Color.ORANGE);
+		btnNewButton_1_1_2_2.setForeground(Color.WHITE);
 		btnNewButton_1_1_2_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -301,6 +345,8 @@ public class Cal {
 		frame.getContentPane().add(btnNewButton_1_1_2_2);
 		
 		JButton btnNewButton_4_2 = new JButton("0");
+		btnNewButton_4_2.setBackground(Color.DARK_GRAY);
+		btnNewButton_4_2.setForeground(Color.WHITE);
 		btnNewButton_4_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String num = textField.getText() + btnNewButton_4_2.getText();
